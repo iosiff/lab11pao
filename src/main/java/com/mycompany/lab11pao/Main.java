d116f065b0629abee607ab122ffb143ee13fc7f3
Fana Iosif <0x1051f@protonmail.com>
2017-05-09 15:47:49 +0300

    Add for with 10 iterations!

--- src/main/java/com/mycompany/lab11pao/Main.java
+++ src/main/java/com/mycompany/lab11pao/Main.java
@@ -15,11 +15,12 @@ package com.mycompany.lab11pao;
 // ...
 
 // Pasi GIT:
-
+// git clone 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!!!");
+        for(int i = 0; i < 10; i++)
+            System.out.println("Iteratia " + i);
     }
 }
 
