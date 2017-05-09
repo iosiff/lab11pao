3370514e618fb735e2f11bf4d93a8a251b964bcf
Fana Iosif <0x1051f@protonmail.com>
2017-05-09 15:40:55 +0300

    Add all files

---
+++ src/main/java/com/mycompany/lab11pao/Main.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.lab11pao;
+
+// ! Saptamana 13 testul si proiectul
+
+// Proiect:
+// 1. using java coding standards
+// 2. using git
+
+// in linia de comanda
+// meniuri ex:
+// 1. Write article
+// 2. Show all articles
+// 3. ...
+// ...
+
+// Pasi GIT:
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!!");
+    }
+}
+
